12f53169a7355eb379a6c7596e32e2f11d744b5b
Stevanie Kanter <stevaniek@yahoo.com>
2021-08-19 18:45:18 -0400

    added function for license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      console.log("https://img.shields.io/badge/license-MIT-blue");
+      break;
+    case "Apache 2.0":
+      console.log("https://img.shields.io/badge/license-Apache%202.0-blue");
+      break;
+    case "GPL 3.0":
+      console.log("https://img.shields.io/badge/license-GPL%203.0-blue");
+      break;
+    case "BSD 3":
+      console.log("https://img.shields.io/badge/license-BSD%203-blue");
+      break;
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+"MIT", "Apache 2.0", "GPL 3.0", "BSD 3";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
 
 `;
 }
