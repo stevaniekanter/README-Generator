c8a41b259303422eef15b4d3c0abfe8acc9f92fe
Stevanie Kanter <stevaniek@yahoo.com>
2021-08-19 17:44:36 -0400

    added questions array for input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,59 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your project title?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Write a brief desription of your project",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Provide any steps required to install the application",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message:
+          "List any examples and instructions for use of the application",
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "Please select a license type",
+        choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "List any contributors you'd like to add",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Would you like to to run any tests for your project?",
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'what is your github username?',
+    },
+      {
+        type: "input",
+        name: "questions",
+        message: "Provide your e-mail address",
+      },
+    ];
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
