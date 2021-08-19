b4cd089dbf4d9d4ba2a1e929794cf81f662dc838
Stevanie Kanter <stevaniek@yahoo.com>
2021-08-19 17:48:09 -0400

    created function to write file

--- Develop/index.js
+++ Develop/index.js
@@ -9,57 +9,68 @@ const questions = [
         type: "input",
         name: "title",
         message: "Please enter your project title?",
-      },
-      {
+    },
+    {
         type: "input",
         name: "description",
         message: "Write a brief desription of your project",
-      },
-      {
+    },
+    {
         type: "input",
         name: "installation",
         message: "Provide any steps required to install the application",
-      },
-      {
+    },
+    {
         type: "input",
         name: "usage",
         message:
-          "List any examples and instructions for use of the application",
-      },
-      {
+            "List any examples and instructions for use of the application",
+    },
+    {
         type: "input",
         name: "license",
         message: "Please select a license type",
         choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
-      },
-      {
+    },
+    {
         type: "input",
         name: "contributing",
         message: "List any contributors you'd like to add",
-      },
-      {
+    },
+    {
         type: "input",
         name: "tests",
         message: "Would you like to to run any tests for your project?",
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'github',
         message: 'what is your github username?',
     },
-      {
+    {
         type: "input",
         name: "questions",
         message: "Provide your e-mail address",
-      },
-    ];
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(data);
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(data);
+        writeToFile("README.md", generateMarkdown({ ...data }));
+    });
+}
 
 // Function call to initialize app
 init();
