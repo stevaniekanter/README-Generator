eb0c86994f6748f1be0c365b3c9bbcb8d052bcc2
Stevanie Kanter <stevaniek@yahoo.com>
2021-08-20 18:08:48 -0400

    added markdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -24,7 +24,38 @@ function renderLicenseBadge(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
 
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Github] (#github)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This application is licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+  
+  ##Github
+  ${data.github}
+
+  ## Questions
+  Please feel free to contact me at ${data.questions} for any questions.
 `;
 }
 
